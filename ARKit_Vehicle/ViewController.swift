ca7f79edeb7bad70d62815f5b3079d824cc86ab0
IJke <ijke@overscope.net>
2018-01-30 12:51:41 +0100

    Final version

--- ARKit_Vehicle/ViewController.swift
+++ ARKit_Vehicle/ViewController.swift
@@ -8,19 +8,38 @@
 
 import UIKit
 import ARKit
+import CoreMotion
 
 class ViewController: UIViewController, ARSCNViewDelegate {
     
     @IBOutlet weak var sceneView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
+    let motionManager = CMMotionManager()
+    var vehicle = SCNPhysicsVehicle()
+    var orientation: CGFloat = 0
+    var touched: Int = 0
+    var accelerationValues = [UIAccelerationValue(0), UIAccelerationValue(0)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.sceneView.delegate = self
         self.configuration.planeDetection = .horizontal
         self.sceneView.session.run(configuration)
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.showsStatistics = true
+        self.setupAccelerometer()
         
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let _ = touches.first else { return }
+        self.touched += touches.count
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.touched = 0
     }
     
     override func didReceiveMemoryWarning() {
@@ -30,8 +49,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
-        let lavaNode = createLava(planeAnchor: planeAnchor)
-        node.addChildNode(lavaNode)
+        let concreteNode = createConcrete(planeAnchor: planeAnchor)
+        node.addChildNode(concreteNode)
         print("New flat surface detected, new ARPlaneAnchor added")
     }
     
@@ -40,8 +59,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         node.enumerateChildNodes { (childNode, _) in
             childNode.removeFromParentNode()
         }
-        let lavaNode = createLava(planeAnchor: planeAnchor)
-        node.addChildNode(lavaNode)
+        let concreteNode = createConcrete(planeAnchor: planeAnchor)
+        node.addChildNode(concreteNode)
         print("Updating plane anchor")
     }
     
@@ -52,20 +71,107 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
     }
     
-    func createLava(planeAnchor: ARPlaneAnchor) -> SCNNode {
-        let lavaNode = SCNNode(geometry: SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z)))
-        lavaNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "lava")
-        lavaNode.position = SCNVector3(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
-        lavaNode.geometry?.firstMaterial?.isDoubleSided = true
-        lavaNode.eulerAngles = SCNVector3(90.degreesToRadians, 0, 0)
-        return lavaNode
+    func createConcrete(planeAnchor: ARPlaneAnchor) -> SCNNode {
+        
+        let concreteNode = SCNNode(geometry: SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z)))
+        concreteNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "concrete")
+        concreteNode.position = SCNVector3(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
+        concreteNode.geometry?.firstMaterial?.isDoubleSided = true
+        concreteNode.eulerAngles = SCNVector3(90.degreesToRadians, 0, 0)
+        
+        let staticBody = SCNPhysicsBody.static()
+        concreteNode.physicsBody = staticBody
+        
+        return concreteNode
     }
     
+    func setupAccelerometer() {
+        if motionManager.isAccelerometerAvailable {
+            motionManager.accelerometerUpdateInterval = 1/60
+            motionManager.startAccelerometerUpdates(to: .main) { (accelerometerData, error) in
+                if let error = error {
+                    print(error.localizedDescription)
+                }
+                self.accelerometerDidChange(acceleration: accelerometerData!.acceleration)
+            }
+        } else {
+            print("No accelerometer available")
+        }
+    }
+    
+    func accelerometerDidChange(acceleration: CMAcceleration) {
+        accelerationValues[1] = filtered(currentAcceleration: accelerationValues[1], updatedAcceleration: acceleration.y)
+        accelerationValues[0] = filtered(currentAcceleration: accelerationValues[0], updatedAcceleration: acceleration.x)
+        if accelerationValues[0] > 0 {
+            self.orientation = CGFloat(accelerationValues[1])
+        } else {
+            self.orientation = -CGFloat(accelerationValues[1])
+        }
+    }
+    
+    func filtered(currentAcceleration: Double, updatedAcceleration: Double) -> Double {
+        let kfilteringFactor = 0.5
+        return updatedAcceleration * kfilteringFactor + currentAcceleration * (1 - kfilteringFactor)
+    }
+    
+    @IBAction func addCar(_ sender: Any) {
+        guard let pointOfView = sceneView.pointOfView else { return }
+        let transform = pointOfView.transform
+        let orientation = SCNVector3(-transform.m31, -transform.m32, -transform.m33)
+        let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+        let currentPositionOfCamera = orientation + location
+        
+        let scene = SCNScene(named: "car-scene.scn")
+        let chassis = (scene?.rootNode.childNode(withName: "chassis", recursively: false))!
+        let frontLeftWheel = chassis.childNode(withName: "frontLeftParent", recursively: false)!
+        let frontRightWheel = chassis.childNode(withName: "frontRightParent", recursively: false)!
+        let rearLeftWheel = chassis.childNode(withName: "rearLeftParent", recursively: false)!
+        let rearRightWheel = chassis.childNode(withName: "rearRightParent", recursively: false)!
+        
+        let v_frontLeftWheel = SCNPhysicsVehicleWheel(node: frontLeftWheel)
+        let v_frontRightWheel = SCNPhysicsVehicleWheel(node: frontRightWheel)
+        let v_rearLeftWheel = SCNPhysicsVehicleWheel(node: rearLeftWheel)
+        let v_rearRightWheel = SCNPhysicsVehicleWheel(node: rearRightWheel)
+        
+        chassis.position = currentPositionOfCamera
+        let body = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: chassis, options: [SCNPhysicsShape.Option.keepAsCompound: true]))
+        body.mass = 1
+        chassis.physicsBody = body
+        self.vehicle = SCNPhysicsVehicle(chassisBody: chassis.physicsBody!, wheels: [v_rearLeftWheel, v_rearRightWheel, v_frontLeftWheel, v_frontRightWheel])
+        self.sceneView.scene.physicsWorld.addBehavior(self.vehicle)
+        self.sceneView.scene.rootNode.addChildNode(chassis)
+        
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didSimulatePhysicsAtTime time: TimeInterval) {
+        var engineForce: CGFloat = 0
+        var brakingForce: CGFloat = 0
+        self.vehicle.setSteeringAngle(orientation, forWheelAt: 2)
+        self.vehicle.setSteeringAngle(orientation, forWheelAt: 3)
+        if self.touched == 1 {
+            engineForce = 5
+        } else if self.touched == 2 {
+            engineForce = -5
+        } else if self.touched == 3 {
+            brakingForce = 100
+        } else {
+            engineForce = 0
+        }
+        
+        self.vehicle.applyEngineForce(engineForce, forWheelAt: 0)
+        self.vehicle.applyEngineForce(engineForce, forWheelAt: 1)
+        self.vehicle.applyBrakingForce(brakingForce, forWheelAt: 0)
+        self.vehicle.applyBrakingForce(brakingForce, forWheelAt: 1)
+    }
     
 }
 
 extension Int {
     var degreesToRadians: Double { return Double(self) * .pi / 180 }
+}
+
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3(left.x + right.x, left.y + right.y, left.z + right.z)
 }
 
 
